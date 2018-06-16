41a89afc64fdaff73472b47d77a0d87c8d8887c3
djiele <ojo.djiele@gmail.com>
2018-06-16 02:15:08 +0200

    no comment

--- skype_web_php/SkypeSession.php
+++ skype_web_php/SkypeSession.php
@@ -1,6 +1,6 @@
 <?php
-require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'Services_JSON.php';
-namespace skype_session_php;
+namespace skype_web_php;
+require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'json.php';
 use \Curl\Curl;
 
 class SkypeSession {
@@ -11,13 +11,25 @@ class SkypeSession {
 		$srvData = substr($srvData, strpos($srvData, '{'));
 		$srvDataEnd = strpos($srvData, ';</script>');
 		$srvData = substr($srvData, 0, $srvDataEnd);
-		$json = new Services_JSON();
+		$json = new \Services_JSON();
 		$srvData = $json->decode($srvData);
 		return $srvData;
 	}
+	
+	static public function getSkypeToken($login, $passwd, $dataPath) {
+		$skypeToken = json_decode(file_get_contents($dataPath.$login.'-session.json'), true);
+		$skypeToken['expires_in'] = (int)$skypeToken['expires_in'];
+		if($skypeToken['expires_in']<time()) {
+			$skypeToken = self::fetchSkypeToken($login, $passwd, $dataPath);
+			$skypeToken['expires_in'] += (time()-3600);
+			if(!file_put_contents($dataPath.$login.'-session.json', json_encode($skypeToken, JSON_PRETTY_PRINT))) {
+				echo 'session file write error' . PHP_EOL;
+			}
+		}
+		return $skypeToken;
+	}
 
-	static public function getSkypeToken($login, $passwd) {
-		$dataPath = getcwd().DIRECTORY_SEPARATOR.'app-data'.DIRECTORY_SEPARATOR;
+	static public function fetchSkypeToken($login, $passwd, $dataPath) {
 		$skypeToken = null;
 		$skypeTokenExpires = null;
 		$curl = new Curl();
@@ -40,7 +52,7 @@ class SkypeSession {
 		$curl->post($urlPost, $postData);
 
 		$response2 = $curl->response;
-		$doc =  new DOMDocument();
+		$doc =  new \DOMDocument();
 		@$doc->loadHTML($response2, LIBXML_NOWARNING | LIBXML_NOERROR);
 		$forms = $doc->getElementsByTagName('form');
 		$urlPost = $forms[0]->getAttribute('action');
@@ -54,7 +66,7 @@ class SkypeSession {
 		$curl->post($urlPost, $postData);
 
 		$response3 = $curl->response;
-		$doc =  new DOMDocument();
+		$doc =  new \DOMDocument();
 		@$doc->loadHTML($response3, LIBXML_NOWARNING | LIBXML_NOERROR);
 		$forms = $doc->getElementsByTagName('form');
 		$urlPost = $forms[0]->getAttribute('action');
@@ -72,7 +84,7 @@ class SkypeSession {
 		$curl->post($urlPost, $postData);
 
 		$response4 = $curl->response;
-		$doc =  new DOMDocument();
+		$doc =  new \DOMDocument();
 		@$doc->loadHTML($response4, LIBXML_NOWARNING | LIBXML_NOERROR);
 		$forms = $doc->getElementsByTagName('form');
 		$urlPost = $forms[0]->getAttribute('action');
